e47d833f7e3ea9a680b9aa64582ed982416fc05c
Danny Hiemstra <dannyhiemstra@gmail.com>
2017-07-16 17:02:30 +0200

    Add helpers

---
+++ app/helpers/output_helper.rb
@@ -0,0 +1,24 @@
+module OutputHelper
+  def isk(number, force_negative=false, round=true)
+    if number
+      number = BigDecimal.new(number) if number.is_a?(String)
+      precision = round ? 0 : 2
+      number = -number if force_negative
+      negative = number < 0
+      isk = number_to_currency(number, unit: 'ISK', separator: '.', delimiter: ',', format: "%n %u", precision: precision)
+      content_tag(:span, isk, class: "isk #{negative ? 'text-danger' : ''}")
+    else
+      '-'
+    end
+  end
+
+  def number(value)
+    number_with_delimiter value
+  end
+
+  def percentage(value)
+    return '-' unless value
+    value = BigDecimal.new(value) if value.is_a?(String)
+    value.round(1).to_s + "%"
+  end
+end
---
+++ app/helpers/type_card_helper.rb
@@ -0,0 +1,17 @@
+module TypeCardHelper
+  def type_card(type, size=:medium, options={})
+    # locals = { type: type, size: size, link_url: nil, clipboard: nil, description: nil }.merge(options)
+    # render partial: 'shared/type_card', locals: locals
+    content_tag(:div, class: "media type-card type-card-#{size}") do
+      [
+        image_tag(type.image, class: "d-flex mr-3"),
+        content_tag(:div, class: "media-body") do
+          [
+            content_tag(:h6, type.name),
+            content_tag(:div, 'description', class: 'text-sm text-muted')
+          ].join("\n")
+        end
+      ].join("\n")
+    end
+  end
+end
