ae9fbfb581162f8ea35d097c7807cfdca69bc60e
Danny Hiemstra <dannyhiemstra@gmail.com>
2017-07-16 21:03:48 +0200

    htmlsafe

--- app/helpers/eve_app/type_card_helper.rb
+++ app/helpers/eve_app/type_card_helper.rb
@@ -2,7 +2,7 @@ module EveApp::TypeCardHelper
   def type_card(type, size=:medium, options={})
     # locals = { type: type, size: size, link_url: nil, clipboard: nil, description: nil }.merge(options)
     # render partial: 'shared/type_card', locals: locals
-    content_tag(:div, class: "media type-card type-card-#{size}") do
+    raw content_tag(:div, class: "media type-card type-card-#{size}") do
       [
         image_tag(type.image, class: "d-flex mr-3"),
         content_tag(:div, class: "media-body") do
@@ -12,6 +12,6 @@ module EveApp::TypeCardHelper
           ].join("\n")
         end
       ].join("\n")
-    end.html_safe
+    end
   end
 end
