9876d58d6864493a40d49fa739f2a76d259c925b
Danny Hiemstra <dannyhiemstra@gmail.com>
2017-07-16 22:14:12 +0200

    Add solar system constantsg

--- app/models/eve_app/solar_system.rb
+++ app/models/eve_app/solar_system.rb
@@ -1,6 +1,14 @@
 class EveApp::SolarSystem < EveApp::ApplicationRecord
+  JITA = 30000142
+  PERIMITER = 30000144
+  NEW_CALDARI = 30000145
+  AMARR = 30002187
+  NIYABAINEN = 30000143
+  P3EN = 30000250
+  ASHAB = 30003491
+  MAURASI = 30000140
+
   belongs_to :region
 
   has_many :stations
 end
-
